45697ef5f4d1ed40009a61475b1db7e53a258c14
YAMAMOTO Masaya <pandax381@gmail.com>
2025-08-19 15:58:43 +0900

    Add hardware interrupt handling for virtio-net

--- kernel/trap.c
+++ kernel/trap.c
@@ -196,6 +196,8 @@ devintr()
       uartintr();
     } else if(irq == VIRTIO0_IRQ){
       virtio_disk_intr();
+    } else if(irq == VIRTIO1_IRQ){
+      virtio_net_intr();
     } else if(irq){
       printf("unexpected interrupt irq=%d\n", irq);
     }
