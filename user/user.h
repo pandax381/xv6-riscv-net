908ebf4f00276ed1dd9609a450b893a8c257da53
YAMAMOTO Masaya <pandax381@gmail.com>
2025-08-19 17:42:48 +0900

    Add sample network applications

--- user/user.h
+++ user/user.h
@@ -47,6 +47,10 @@ void* memset(void*, int, uint);
 int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
+uint16_t htons(uint16_t);
+uint16_t ntohs(uint16_t);
+uint32_t htonl(uint32_t);
+uint32_t ntohl(uint32_t);
 
 // umalloc.c
 void* malloc(uint);
